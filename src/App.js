aefb49985e5c1821f84744f6dfff338c76213617
Alessio Carusio <alessiocarusio@gmail.com>
2022-11-04 12:56:58 +0100

    Navigazione e tabella funzionanti

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Suspense, lazy } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+
+// import Dashboard from './pages/Dashboard.jsx';
+// import About from './pages/About.jsx';
+// import Analytics from './pages/Analytics.jsx';
+// import Comment from './pages/Comment.jsx';
+// import Product from './pages/Product.jsx';
+// import ProductList from './pages/ProductList.jsx';
+// import Login from './pages/Login';
+
+
+const Dashboard = lazy(() => import('./pages/Dashboard.jsx'));
+const About = lazy(() => import('./pages/About.jsx'));
+const Analytics = lazy(() => import('./pages/Analytics.jsx'));
+const Comment = lazy(() => import('./pages/Comment.jsx'));
+const Product = lazy(() => import('./pages/Product.jsx'));
+const ProductList = lazy(() => import('./pages/ProductList.jsx'));
+const Login = lazy(() => import('./pages/Login.jsx'));
+
+
+
+
+const logged = false;
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Sidebar>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={logged ? <Dashboard /> : <Login /> } />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+        </Suspense>
+      </Sidebar>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,212 @@
+import React, {useState, useEffect} from 'react';
+import MaterialTable from 'material-table';
+import { ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+
+import { Alert } from '@mui/material';
+import AlertTitle from '@mui/material/AlertTitle';
+import axios from 'axios';
+
+
+
+
+
+// export default function About() {
+//     return (
+//         <div>
+//             <p>ciao</p>
+//         </div>
+//     );
+// };
+
+
+const validateEmail = (email) => {
+    const re = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+export default function About(){
+    const defaultMaterialTheme = createTheme();
+    const [user, setUser] = useState([]);
+  const [iserror, setIserror] = useState(false);
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  let columns = [
+    { title: 'NAME', field: 'name' },
+    { title: 'USERNAME', field: 'username' },
+    { title: 'EMAIL', field: 'email' },
+    { title: 'PHONE', field: 'phone' },
+    { title: 'WEBSITE', field: 'website' },
+  ]
+
+  let data = [
+    { name: 'manish', username: 'traptrick', email: 'themk85@gmail.com', phone: '9999999999', website: 'https://github.com/traptrick' }
+  ]  
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const users = res.data;
+        setUser(users);
+        // console.log(users); 
+      })
+  }, [])
+
+
+
+  //function for updating the existing row details
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    //validating the data inputs
+    let errorList = []
+    if (newData.name === "") {
+      errorList.push("Try Again, You didn't enter the name field")
+    }
+    if (newData.username === "") {
+      errorList.push("Try Again, You didn't enter the Username field")
+    }
+    if (newData.email === "" || validateEmail(newData.email) === false) {
+      errorList.push("Oops!!! Please enter a valid email")
+    }
+    if (newData.phone === "") {
+      errorList.push("Try Again, Phone number field can't be blank")
+    }
+    if (newData.website === "") {
+      errorList.push("Try Again, Enter website url before submitting")
+    }
+
+    if (errorList.length < 1) {
+      axios.put(`https://jsonplaceholder.typicode.com/users/${newData.id}`, newData)
+        .then(response => {
+          const updateUser = [...user];
+          const index = oldData.tableData.id;
+          updateUser[index] = newData;
+          setUser([...updateUser]);
+          resolve()
+          setIserror(false)
+          setErrorMessages([])
+        })
+        .catch(error => {
+          setErrorMessages(["Update failed! Server error"])
+          setIserror(true)
+          resolve()
+
+        })
+    } else {
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+
+    }
+  }
+
+
+  //function for deleting a row
+  const handleRowDelete = (oldData, resolve) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${oldData.id}`)
+      .then(response => {
+        const dataDelete = [...user];
+        const index = oldData.tableData.id;
+        dataDelete.splice(index, 1);
+        setUser([...dataDelete]);
+        resolve()
+      })
+      .catch(error => {
+        setErrorMessages(["Delete failed! Server error"])
+        setIserror(true)
+        resolve()
+      })
+  }
+
+
+  //function for adding a new row to the table
+  const handleRowAdd = (newData, resolve) => {
+    //validating the data inputs
+    let errorList = []
+    if (newData.name === "") {
+      errorList.push("Try Again, You didn't enter the name field")
+    }
+    if (newData.username === "") {
+      errorList.push("Try Again, You didn't enter the Username field")
+    }
+    if (newData.email === "" || validateEmail(newData.email) === false) {
+      errorList.push("Oops!!! Please enter a valid email")
+    }
+    if (newData.phone === "") {
+      errorList.push("Try Again, Phone number field can't be blank")
+    }
+    if (newData.website === "") {
+      errorList.push("Try Again, Enter website url before submitting")
+    }
+
+    if (errorList.length < 1) {
+      axios.post(`https://jsonplaceholder.typicode.com/users`, newData)
+        .then(response => {
+          let newUserdata = [...user];
+          newUserdata.push(newData);
+          setUser(newUserdata);
+          resolve()
+          setErrorMessages([])
+          setIserror(false)
+        })
+        .catch(error => {
+          setErrorMessages(["Cannot add data. Server error!"])
+          setIserror(true)
+          resolve()
+        })
+    } else {
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+    }
+  }
+
+
+  return(
+
+    <div>
+    <div style={{ width: '100%', height: '100%' }}>
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            />
+        <ThemeProvider theme={defaultMaterialTheme}>
+        <MaterialTable
+        title="User Details"
+        columns={columns}
+         data={user}
+        options={{
+          headerStyle: { borderBottomColor: 'red', borderBottomWidth: '3px', fontFamily: 'verdana' },
+          actionsColumnIndex: -1
+        }}
+        editable={{
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve) => {
+                handleRowUpdate(newData, oldData, resolve);
+  
+              }),
+            onRowAdd: (newData) =>
+              new Promise((resolve) => {
+                handleRowAdd(newData, resolve)
+              }),
+            onRowDelete: (oldData) =>
+              new Promise((resolve) => {
+                handleRowDelete(oldData, resolve)
+              }),
+          }}
+        />
+        </ThemeProvider>
+    </div>
+
+<div>
+{iserror &&
+  <Alert severity="error">
+    <AlertTitle>ERROR</AlertTitle>
+    {errorMessages.map((msg, i) => {
+      return <div key={i}>{msg}</div>
+    })}
+  </Alert>
+}
+</div>
+</div>
+);
+ }

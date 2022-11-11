ca603edd056a527aba0357279c67621e2b36253c
Alessio Carusio <alessiocarusio@gmail.com>
2022-11-11 17:45:24 +0100

    Merged Dialogs

---
+++ src/components/Dialogs/DialogSpettacolo.jsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+
+import { useState } from "react";
+
+import { InputAdornment } from "@mui/material";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/styles";
+import axios from "axios";
+import { useEffect } from "react";
+import { MenuItem } from "@mui/material";
+
+const CustomTextField = styled(TextField)({
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "white",
+      },
+    },
+    "&   .MuiSelect-icon": {
+      color: "#F9D159",
+    },
+  });
+
+
+export default function DialogSpettacolo(){
+    const [film, setFilm] = useState([]);
+    const [sale, setSale] = useState([]);
+    useEffect(() => {
+        axios
+          .get(`https://0ptix34dk9.execute-api.eu-central-1.amazonaws.com/film`)
+          .then((res) => {
+            setFilm(res.data);
+          });
+      }, []);
+    
+      useEffect(() => {
+        axios
+          .get(`https://0ptix34dk9.execute-api.eu-central-1.amazonaws.com/1/sale`)
+          .then((res) => {
+            setSale(res.data);
+          });
+      }, []);
+    return(
+    <Box> 
+    <CustomTextField
+    margin="normal"
+    defaultValue=""
+    select
+    name="film"
+    fullWidth
+    label="Seleziona Film"
+  >
+    {film.map((film, index) => (
+      <MenuItem key={index} value={JSON.stringify(film)}>
+        {film.titolo}
+      </MenuItem>
+    ))}
+  </CustomTextField>
+
+  <CustomTextField
+    margin="normal"
+    defaultValue=""
+    select
+    name="sala"
+    fullWidth
+    label="Seleziona Sala"
+  >
+    {sale.map((sala, index) => (
+      <MenuItem key={index} value={JSON.stringify(sala)}>
+        {sala.numero_sala}
+      </MenuItem>
+    ))}
+  </CustomTextField>
+
+  <CustomTextField
+    label="Data spettacolo"
+    id="standard-start-adornment"
+    InputProps={{
+      startAdornment: <InputAdornment position="start" />,
+    }}
+    type="datetime-local"
+    fullWidth
+    margin="normal"
+    name="data"
+  />
+
+  <CustomTextField
+    margin="normal"
+    fullWidth
+    label="Prezzo biglietto"
+    type="number"
+    name="prezzo"
+  />
+
+</Box>
+);
+}
---
+++ src/components/InsertFormDialog.jsx
@@ -0,0 +1,251 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import { useState } from "react";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Button, Container, Stack } from "@mui/material";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/styles";
+import axios from "axios";
+
+import DialogSpettacolo from "./Dialogs/DialogSpettacolo";
+import DialogFilm from "./Dialogs/DialogFilm";
+import DialogSala from "./Dialogs/DialogSala";
+
+
+export default function InsertFormDialog(props) {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [iserror, setIserror] = useState(false);
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  const handleClose = () => {
+    props.setCloseDialog();
+  };
+
+
+  const handleSubmit = (event) => {
+    if (props.formType === "film") {
+      setIsDisabled(true);
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      const newFilm = {
+        titolo: data.get("titolo"),
+        genere: data.get("genere"),
+        cast: data.get("cast"),
+        regia: data.get("regia"),
+        produttore: data.get("produttore"),
+        data_uscita: data.get("data"),
+        durata: data.get("durata"),
+        trama: data.get("trama"),
+        image_url: data.get("locandina"),
+      };
+
+      axios
+        .post(
+          `https://0ptix34dk9.execute-api.eu-central-1.amazonaws.com/film`,
+          newFilm
+        )
+        .then((response) => {
+          const newFilm2 = {
+            codice_film: response.data.codice_film,
+            titolo: data.get("titolo"),
+            genere: data.get("genere"),
+            cast: data.get("cast"),
+            regia: data.get("regia"),
+            produttore: data.get("produttore"),
+            data_uscita: data.get("data"),
+            durata: data.get("durata"),
+            trama: data.get("trama"),
+            image_url: data.get("locandina"),
+          };
+
+          
+          props.onAddFilm(newFilm2);
+
+          setIsDisabled(false);
+          handleClose();
+          setIserror(false);
+          setErrorMessages([]);
+        })
+        .catch((error) => {
+          console.log(error);
+          setErrorMessages(["Update failed! Server error"]);
+          setIserror(true);
+        });
+
+      setIsDisabled(false);
+    } else if (props.formType === "sala") {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      console.log({
+        titolo: data.get(""),
+      });
+    } else if (props.formType === "spettacolo") {
+      setIsDisabled(true);
+      event.preventDefault();
+
+      const data = new FormData(event.currentTarget);
+
+      const jsonData = {
+        codice_film: JSON.parse(data.get("film")).codice_film,
+        id_sala: JSON.parse(data.get("sala")).id_sala,
+        data_ora: data.get("data"),
+        prezzo: data.get("prezzo"),
+      };
+
+      axios
+        .post(
+          `https://0ptix34dk9.execute-api.eu-central-1.amazonaws.com/spettacolo`,
+          jsonData
+        )
+        .then((response) => {
+          const sala = JSON.parse(data.get("sala"));
+          const film = JSON.parse(data.get("film"));
+
+          const newSpettacolo = {
+            codice_spettacolo: response.data.codice_spettacolo,
+            data: data.get("data").replace(/T/, " "),
+            prezzo: data.get("prezzo"),
+            sala: {
+              id_sala: sala.id_sala,
+              numero_sala: sala.numero_sala,
+            },
+            film: {
+              codice_film: film.codice_film,
+              titolo: film.titolo,
+              durata: film.durata,
+            },
+          };
+
+          props.onAddSpettacolo(newSpettacolo);
+          setIsDisabled(false);
+          handleClose();
+          setIserror(false);
+          setErrorMessages([]);
+        })
+        .catch((error) => {
+          setErrorMessages(["Update failed! Server error"]);
+          setIserror(true);
+        });
+    }
+  };
+
+  if (props.formType === "film") {
+    return (
+      <div>
+        <Dialog
+          open={props.openDialog}
+          fullWidth
+          maxWidth="sm"
+          onClose={handleClose}
+        >
+          <DialogTitle align="center">Inserisci dettagli film</DialogTitle>
+          <DialogContent>
+            <Container component="main" maxWidth="xs">
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{
+                  alignItems: "center",
+                  display: "flex",
+                  flexDirection: "column",
+                  "& .MuiTextField-root": { width: "50ch" },
+                }}
+              >
+               <DialogFilm/>
+                <p></p>
+                <Stack direction="row" spacing={2}>
+                  <Button
+                    variant="outlined"
+                    type="submit"
+                    disabled={isDisabled}
+                  >
+                    Ok
+                  </Button>
+                  <Button variant="outlined" onClick={handleClose}>
+                    Cancel
+                  </Button>
+                </Stack>
+              </Box>
+            </Container>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+  } else if (props.formType === "sala") {
+    return (
+      <div>
+        <Dialog
+          open={props.openDialog}
+          fullWidth
+          maxWidth="sm"
+          onClose={handleClose}
+        >
+          <DialogTitle align="center">Inserisci Sala</DialogTitle>
+          <DialogContent>
+            <Container component="main" maxWidth="xs">
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{
+                  alignItems: "center",
+                  display: "flex",
+                  flexDirection: "column",
+                  "& .MuiTextField-root": { width: "50ch" },
+                }}
+              >
+
+                <DialogSala/>
+               
+
+                <p></p>
+                <Stack direction="row" spacing={2}>
+                  <Button variant="outlined" type="submit">
+                    Ok
+                  </Button>
+                  <Button variant="outlined" onClick={handleClose}>
+                    Cancel
+                  </Button>
+                </Stack>
+              </Box>
+            </Container>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+  } else if (props.formType === "spettacolo") {
+    return (
+      <div>
+        <Dialog
+          open={props.openDialog}
+          fullWidth
+          maxWidth="sm"
+          onClose={handleClose}
+        >
+          <DialogTitle align="center">Inserisci Spettacolo</DialogTitle>
+          <DialogContent>
+            <Container component="main" maxWidth="xs">
+              <Box component="form" onSubmit={handleSubmit}>
+               <DialogSpettacolo/>
+                <p></p>
+                <Stack direction="row" spacing={2}>
+                  <Button
+                    variant="outlined"
+                    type="submit"
+                    disabled={isDisabled}
+                  >
+                    Ok
+                  </Button>
+                  <Button variant="outlined" onClick={handleClose}>
+                    Cancel
+                  </Button>
+                </Stack>
+              </Box>
+            </Container>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+  }
+}

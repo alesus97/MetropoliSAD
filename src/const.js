a585e68e1513d849492a991040ce9ab07094bb72
Alessio Carusio <alessiocarusio@gmail.com>
2022-11-14 13:28:36 +0100

    Added Amplify config

---
+++ src/const.js
@@ -0,0 +1,1 @@
+export const AUTH_USER_TOKEN_KEY = 'ReactAmplify.TokenKey';
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import {Avatar,
+import {
+  Avatar,
   Button,
   TextField,
   FormControlLabel,
@@ -9,12 +10,19 @@ import {Avatar,
   Box,
   Typography,
   Container,
-} from '@mui/material'
+  Alert,
+} from "@mui/material";
+import { useState } from "react";
+import { Error } from "@mui/icons-material";
 
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import { useNavigate } from "react-router-dom";
+import { Amplify, Auth } from "aws-amplify";
+import awsconfig from "../aws-exports";
+import { AUTH_USER_TOKEN_KEY } from "../const";
 
-import {styled } from "@mui/styles";
+import { styled } from "@mui/styles";
+
 
 function Copyright(props) {
   return (
@@ -34,99 +42,131 @@ function Copyright(props) {
   );
 }
 
-
 const CustomTextField = styled(TextField)({
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: 'white',
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "white",
     },
-}})
+  },
+});
 
 export default function Login(props) {
-
+  Amplify.configure(awsconfig);
+  const [iserror, setIserror] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-    props.onLoginAction();
-    navigate("/palinsesto", { replace: true });
+    const email = data.get("email");
+    const password = data.get("password");
+
+
+
+    Auth.signIn(email, password)
+      .then((user) => {
+        setIserror(false);
+        setErrorMessage("");
+        console.log(user);
+
+        localStorage.setItem(
+          AUTH_USER_TOKEN_KEY,
+          user.signInUserSession.accessToken.jwtToken
+        );
+        console.log(localStorage);
+
+        props.onLoginAction();
+        navigate("/palinsesto", { replace: true });
+      })
+      .catch((err) => {
+        setErrorMessage(err.message);
+        setIserror(true);
+      });
   };
 
-
   return (
-      <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
         <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
+          component="form"
+          onSubmit={handleSubmit}
+          // noValidate
+          sx={{ mt: 1 }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            // noValidate
-            sx={{ mt: 1 }}
-          >
-            <CustomTextField
-              margin="normal"
-              //required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <CustomTextField
-              margin="normal"
-              //required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+          <CustomTextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <CustomTextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          {iserror ? (
+            <Alert
+              variant="filled"
+              color="primary"
+              icon={<Error fontSize="inherit" />}
             >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+              {errorMessage}
+            </Alert>
+          ) : (
+            <></>
+          )}
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
             </Grid>
-          </Box>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 }

3940f84aae531a5567e6dbc319e2fc603ee5b8c3
Alessio Carusio <alessiocarusio@gmail.com>
2022-12-06 00:49:56 +0100

    Implemented identity pool id

--- src/apis/APIService.js
+++ src/apis/APIService.js
@@ -1,194 +1,231 @@
-import { SnoozeOutlined } from "@mui/icons-material";
 import axios from "axios";
-import { Location } from "react-router-dom";
+import { sign } from "aws4";
 
-const axiosInstance = axios.create({
-    baseURL: 'https://0ptix34dk9.execute-api.eu-central-1.amazonaws.com'
- });
- 
- axiosInstance.defaults.headers.common['Authorization'] = localStorage.getItem("ReactAmplify.TokenKey")
-
-
-  axiosInstance.interceptors.response.use((response) => {
+/* axiosInstance.interceptors.response.use(
+  (response) => {
     return response;
-}, (error) => {
-    
-        console.log(error)
-      if(error.response?.status === 401) {
-        localStorage.removeItem("ReactAmplify.TokenKey");
-        localStorage.removeItem("roles");
-        window.location.reload(true);
-   } else return Promise.reject(error);
+  },
+  (error) => {
+    console.log(error);
+    if (error.response?.status === 401) {
+      localStorage.removeItem("ReactAmplify.TokenKey");
+      localStorage.removeItem("roles");
+      window.location.reload(true);
+    } else return Promise.reject(error);
+  }
+);
+ */
 
-}); 
 
+function signRequest(request) {
+  const { method, url, data } = request;
 
- export const APIService = {
-    /* *
+  const baseRequest = {
+    host: "0ptix34dk9.execute-api.eu-central-1.amazonaws.com",
+    method: method,
+    url: "https://0ptix34dk9.execute-api.eu-central-1.amazonaws.com" + url,
+    path: url,
+    data: data
+  };
+
+  const identityPoolCredentials = JSON.parse(
+    localStorage.getItem("IdentityPoolCredentials")
+  );
+  
+  const signedRequest = sign(baseRequest, {
+    accessKeyId: identityPoolCredentials.AccessKeyId,
+    secretAccessKey: identityPoolCredentials.SecretKey,
+    sessionToken: identityPoolCredentials.SessionToken,
+  });
+
+  delete signedRequest.headers["Host"];
+  delete signedRequest.headers["Content-Length"];
+
+  return signedRequest;
+}
+
+export const APIService = {
+  /* *
         ! Palinsesto
         * OK funziona
     */
-    
-    getAllSpettacoli: function(/* codiceCinema */) {
-        return axiosInstance.request({
-            method: "GET",
-            url: `/1/spettacoli`
-        })
-    },
 
-    deleteSpettacolo: function(codiceSpettacolo) {
-        return axiosInstance.request({
-            method: "DELETE",
-            url: `/spettacolo/${codiceSpettacolo}`
-        });
-    },
+  getAllSpettacoli: function (/* codiceCinema */) {
+    const request = {
+      method: "GET",
+      url: `/1/spettacoli`,
+    };
+    return axios(signRequest(request));
+  },
 
-    createSpettacolo: function(spettacolo) {
-        return axiosInstance.request({
-            method: "POST",
-            url: `/spettacolo`,
-            data: spettacolo
-        });
-    },
+  deleteSpettacolo: function (codiceSpettacolo) {
+    const request = {
+      method: "DELETE",
+      url: `/spettacolo/${codiceSpettacolo}`,
+    };
+    return axios(signRequest(request));
+  },
 
-       /* *
+  createSpettacolo: function (spettacolo) {
+    const request = {
+      method: "POST",
+      url: `/spettacolo`,
+      data: spettacolo,
+    };
+    return axios(signRequest(request));
+  },
+
+  /* *
         ! Sala
         * OK funziona
     */
 
-        getAllSale: function(/* codiceCinema */) {
-            return axiosInstance.request({
-                method: "GET",
-                url: `/1/sale`
-            });
-        },
+  getAllSale: function (/* codiceCinema */) {
+    const request = {
+      method: "GET",
+      url: `/1/sale`,
+    };
+    return axios(signRequest(request));
+  },
 
-        deleteSala: function(idSala) {
-            return axiosInstance.request({
-                method: "DELETE",
-                url: `/sala/${idSala}`
-            });
-        },
+  deleteSala: function (idSala) {
+    const request = {
+      method: "DELETE",
+      url: `/sala/${idSala}`,
+    };
+    return axios(signRequest(request));
+  },
 
-        createSala: function(sala) {
-            return axiosInstance.request({
-                method: "POST",
-                url: `/1/sala`,
-                data: sala
-            });
-        },
+  createSala: function (sala) {
+    const request = {
+      method: "POST",
+      url: `/1/sala`,
+      data: sala,
+    };
+    return axios(signRequest(request));
+  },
 
+  /**
+   *  ! Film
+   *  * OK funziona
+   */
 
+  getAllFilms: function () {
 
-    /** 
-     *  ! Film
-     *  * OK funziona
-    */
+    const request = {
+        method: "GET",
+        url: `/film`,
+      };
 
-     getAllFilms: function() {
-        return axiosInstance.request({
-            method: "GET",
-            url: `/film`
-        });
-    },
+    return axios(signRequest(request));
+  },
 
-    deleteFilm: function(codiceFilm) {
-        return axiosInstance.request({
-            method: "DELETE",
-            url: `/film/${codiceFilm}`
-        });
-    },
+  deleteFilm: function (codiceFilm) {
+    
+    const request = {
+        method: "DELETE",
+        url: `/film/${codiceFilm}`
+    }
+      
+    return axios(signRequest(request));
+  
+  },
 
-    createFilm: function(film) {
-        return axiosInstance.request({
-            method: "POST",
-            url: `/film`,
-            data: film
-        });
-    },
+  createFilm: function (film) {
+    const request = {
+      method: "POST",
+      url: `/film`,
+      data: film,
+    };
+    return axios(signRequest(request));
+  },
 
+  /**
+   *  ! Questions
+   *  *OK funziona
+   */
 
-    /** 
-     *  ! Questions
-     *  *OK funziona
-    */
+  getAllQuestions: function (filmId) {
+    const request = {
+      method: "GET",
+      url: `/${filmId}/domande`,
+    };
+    return axios(signRequest(request));
+  },
 
-     getAllQuestions: function(filmId) {
-        return axiosInstance.request({
-            method: "GET",
-            url: `/${filmId}/domande`
-        });
-    },
+  deleteQuestion: function (codiceDomanda) {
+    const request = {
+      method: "DELETE",
+      url: `/domanda/${codiceDomanda}`,
+    };
+    return axios(signRequest(request));
+  },
 
-    deleteQuestion: function(codiceDomanda) {
-        return axiosInstance.request({
-            method: "DELETE",
-            url: `/domanda/${codiceDomanda}`
-        });
-    },
+  createQuestion: function (filmId, question) {
+    const request = {
+      method: "POST",
+      url: `${filmId}/domanda`,
+      data: question,
+    };
+    return axios(signRequest(request));
+  },
 
-    createQuestion: function(filmId, question) {
-        return axiosInstance.request({
-            method: "POST",
-            url: `${filmId}/domanda`,
-            data: question
-        });
-    },
+  /**
+   *  ! Store
+   *  ? Non implementato
+   */
 
-     /** 
-     *  ! Store
-     *  ? Non implementato
-    */
+  getAllPrizes: function () {
+    const request = {
+      method: "GET",
+      url: `/premi`,
+    };
+    return axios(signRequest(request));
+  },
 
+  deletePrize: function (codicePremio) {
+    const request = {
+      method: "DELETE",
+      url: `/premio/${codicePremio}`,
+    };
+    return axios(signRequest(request));
+  },
 
-      getAllPrizes: function() {
-        return axiosInstance.request({
-            method: "GET",
-            url: `/premi`
-        });
-    },
+  createPrize: function (prize) {
+    const request = {
+      method: "POST",
+      url: `/premio`,
+      data: prize,
+    };
+    return axios(signRequest(request));
+  },
 
-    deletePrize: function(codicePremio) {
-        return axiosInstance.request({
-            method: "DELETE",
-            url: `/premio/${codicePremio}`
-        });
-    },
+  /**
+   *  ! Cinema
+   */
 
-    createPrize: function(prize) {
-        return axiosInstance.request({
-            method: "POST",
-            url: `/premio`,
-            data: prize
-        });
-    },
+  getAllCinemas: function () {
+    const request = {
+      method: "GET",
+      url: `/cinema`,
+    };
+    return axios(signRequest(request));
+  },
 
+  deleteCinema: function (codiceCinema) {
+    const request = {
+      method: "DELETE",
+      url: `/cinema/${codiceCinema}`,
+    };
+    return axios(signRequest(request));
+  },
 
-     /** 
-     *  ! Cinema
-    */
-
-      getAllCinemas: function() {
-        return axiosInstance.request({
-            method: "GET",
-            url: `/cinema`
-        });
-    },
-
-    deleteCinema: function(codiceCinema) {
-        return axiosInstance.request({
-            method: "DELETE",
-            url: `/cinema/${codiceCinema}`
-        });
-    },
-
-    createCinema: function(cinema) {
-        return axiosInstance.request({
-            method: "POST",
-            url: `/cinema`,
-            data: cinema
-        });
-    },
-
-
-}
+  createCinema: function (cinema) {
+    const request = {
+      method: "POST",
+      url: `/cinema`,
+      data: cinema,
+    };
+    return axios(signRequest(request));
+  },
+};

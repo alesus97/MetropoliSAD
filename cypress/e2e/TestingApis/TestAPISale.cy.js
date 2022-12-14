effb06958453ec3467630eaf832845ce5b22f6b6
Giovanni3606 <giovanniled@gmail.com>
2022-12-14 10:13:56 +0100

    Implementati test

---
+++ cypress/e2e/TestingApis/TestAPISale.cy.js
@@ -0,0 +1,62 @@
+import { API, Amplify, Auth } from "aws-amplify";
+import { APIService } from "../../../src/apis/APIService";
+
+import awsmobile from "../../../src/aws-exports";
+
+Amplify.configure(awsmobile);
+const apiName = "cinema-sad";
+
+var onDeleteIndex = null;
+
+before(async ()=>{
+    await Auth.signIn('alesus97@gmail.com','Gabeweaver96!')
+})
+
+    describe('SALE', ()=>{
+    
+    it('GET', async function() { 
+            APIService.getAllSale().then((res)=>{
+                expect(res.status===200 ).to.be.true
+                console.log(res)
+            }).catch((error)=>{
+                console.log(error)
+            })     
+        })
+    
+    
+    it('POST - DELETE', async function() { 
+
+        const sala = {
+            numeroSala: '10',
+            numeroFile: 2,
+            postiPerFila: 2,
+          };
+        const codiceCinema='10'
+        
+
+        APIService.createSala(codiceCinema, sala).then((res)=>{
+             expect(res.status===200).to.be.true
+             console.log(res)
+             onDeleteIndex = res.data.id_sala
+             console.log(onDeleteIndex)  
+               APIService.deleteSala(onDeleteIndex).then((res)=>{
+                  expect(res.status===200).to.be.true
+                  console.log(res)
+                    
+                }).catch((error)=>{
+                    console.log(error)
+                })  
+        //console.log(onDeleteIndex)
+        }).catch((error)=>{
+            console.log(error)
+        })
+                
+    })
+})
+
+        
+   
+
+
+
+
